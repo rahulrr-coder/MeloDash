e85381f0de7db204da675daa3e2657ff9e6808ae
rahulrr-coder <rahulrr5002@protonmail.com>
2025-03-30 10:30:39 +0530

    Create constant for keys

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const LANES = [
+  { key: "ArrowLeft", label: "←", color: "bg-red-500" },
+  { key: "ArrowDown", label: "↓", color: "bg-blue-500" },
+  { key: "ArrowRight", label: "→", color: "bg-green-500" },
+  { key: "ArrowUp", label: "↑", color: "bg-yellow-500" }
+];
+
